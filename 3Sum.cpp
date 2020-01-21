ca1c6e2d57a9bd681fb8d65543a771dbb38b5d94
JamesHsu <james19991102@gmail.com>
2020-01-21 22:15:55 +0800

    Medium hardness

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int s = nums.size();
+        /** Note that the size might less than 3 **/
         if(s < 3) { return {}; }
         sort(nums.begin(), nums.end());
         vector<vector<int>> ret;
         for(int i = 0; i < s-2; ++i) {
+            /** Note that we should skip the same fix number**/
             if(i > 0 && nums[i] == nums[i-1]) {continue;}
             int front = i+1, tail = s-1;
             int sum;
@@ -18,6 +20,7 @@ public:
                 } else {
                     ret.push_back({nums[i], nums[front], nums[tail]});
                     front++, tail--;
+                    /** skip the same numbers **/
                     while(front < tail && nums[front] == nums[front-1]) {front++;}
                     while(front < tail && nums[tail] == nums[tail+1]) {tail--;}
                 }
