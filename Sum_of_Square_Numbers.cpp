61c8d0de0dab9a9e605cf114de4d0eb901f56fdf
JamesHsu <james19991102@gmail.com>
2020-01-23 12:14:11 +0800

    prob633 Easy hardness

--- Sum_of_Square_Numbers.cpp
+++ Sum_of_Square_Numbers.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
+        /** Note that the overflow problem **/
         long left = 0, right = (long)sqrt(c);        
         while(left <= right) {
             long ss = left*left + right*right;
