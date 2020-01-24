edee23a3dfcbfdd8ffc1dbf760d2d23973a72f34
JamesHsu <james19991102@gmail.com>
2020-01-24 13:27:57 +0800

    prob141 Easy hardness

--- Linked_List_Cycle.cpp
+++ Linked_List_Cycle.cpp
@@ -6,6 +6,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// Solution 1: (reverse linked list)
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
@@ -23,6 +24,36 @@ public:
         /** it goes back to head if there's a cycle **/
         if(cur == head) {
             return true;
+        }
+        return false;
+    }
+};
+// Solution 2: (hash set)
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode *> s;
+        while(head) {
+            if(s.find(head) != s.end()) {   // in the set!
+                return true;
+            } else {
+                s.insert(head);
+            }
+            head = head->next;
+        }
+        return false;
+    }
+};
+// Solution 3: (two ptr: fast and slow)
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next) {return false;}
+        ListNode *fast = head, *slow = head;
+        while(fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow) {return true;}
         }
         return false;
     }
