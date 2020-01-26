93121ec73ed6c066f610eaa082d254d465ea3891
JamesHsu <james19991102@gmail.com>
2020-01-27 01:02:18 +0800

    prob347 Medium hardness

---
+++ Top_K_Frequent_Elements.cpp
@@ -0,0 +1,30 @@
+class Compare {
+    public:
+        bool operator()(pair<int,int> p1, pair<int,int> p2) 
+        {
+            return p1.second > p2.second;
+        }
+};
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> umap;
+        for(int n:nums) {
+            umap[n]++;
+        }
+        priority_queue<pair<int,int>, vector<pair<int,int>>, Compare> pq;
+        for(pair<int,int> it:umap) {
+            pq.push(it);
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+        vector<int> ret;
+        while(!pq.empty()) {
+            int t = (pq.top()).second;
+            ret.push_back(t);
+            pq.pop();
+        }
+        return ret;
+    }
+};
