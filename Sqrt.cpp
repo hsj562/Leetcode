6906e7d49017376ed222c91b9e5550e671aa42f7
JamesHsu <james19991102@gmail.com>
2020-02-01 11:17:13 +0800

    prob69 Easy hardness

--- Sqrt.cpp
+++ Sqrt.cpp
@@ -3,6 +3,7 @@ private:
     int bsearch(int l, int r, int x) {
         /** Note that it may overflow **/
         long long mid = (l+r)/2;
+        /** we can also use if(x/mid == mid) to prevent overflow **/
         if(mid * mid < x && (mid+1) * (mid+1) <= x ) {
             return bsearch(mid+1, r, x);
         } else if(mid * mid > x) {
