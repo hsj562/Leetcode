d3848aa2335d8c189a89f5de2cdf7d3a3cd9eaaa
JamesHsu <james19991102@gmail.com>
2020-02-26 14:03:01 +0800

    prob279 Medium hardness

--- Perfect_Squares.cpp
+++ Perfect_Squares.cpp
@@ -1,3 +1,4 @@
+/** BFS **/
 class Solution {
 public:
     int numSquares(int n) {
@@ -16,5 +17,20 @@ public:
             }
         }
         return -1;  // this should not happen
+    }
+};
+/** DP **/
+class Solution {
+public:
+    int numSquares(int n) {
+        int dp[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; ++i) {
+            dp[i] = i;
+            for(int j = 1; j * j <= i; ++j) 
+                dp[i] = min(dp[i], 1+dp[i-j*j]);
+        }
+        return dp[n];
     }
 };
