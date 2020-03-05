191f46f1ae9a4cc6ceba9f9eedbbd05397978fb2
JamesHsu <james19991102@gmail.com>
2020-03-06 01:32:10 +0800

    prob309 Medium hardness

---
+++ Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.empty()) return 0;
+        const int n = prices.size();
+        vector<int> buy(n+1, 0), sell(n+1, 0);
+        buy[1] = -prices[0];    // note the init
+        for(int i = 2; i <= n; ++i) {   
+            buy[i] = max(sell[i-2]-prices[i-1], buy[i-1]);  // key
+            sell[i] = max(sell[i-1], buy[i-1]+prices[i-1]);
+        }       
+        return sell[n];
+    }
+};
